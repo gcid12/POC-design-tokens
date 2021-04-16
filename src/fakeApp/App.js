975332bf5b723025b661c6af75e79c67d6e799a7
gcid12 <gcid12@gmail.com>
2021-04-15 21:32:27 -0700

    Install material core

---
+++ src/fakeApp/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Button } from '@material-ui/core';
+
+const App=()=>{
+
+  return(
+    <div>
+      <h5>Hello I'm the App</h5>
+      <Button variant="outlined" >Hello Friends</Button>
+    </div>
+  )
+
+}
+
+export default App;
